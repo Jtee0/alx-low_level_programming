5b490f3f9c63caddd2915fe4ca06c8cf531b4c43
Jtee0 <Teanahsure@gmail.com>
2022-11-03 00:37:18 -0700

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * is_prime_number - looks for prime
+ * @n: the number
+ * Return: 1 if orime, 0 if not
+ */
+int is_prime_number(int n)
+{
+if (n < 2)
+return (0);
+if (n % 2 == 0)
+return (0);
+else
+	return (_isitprime(n, n / 2));
+}
